bd37e831c5cdbe702dd1881081a68f4334df6665
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-06-22 15:46:44 +0530

    About and qualifications done

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -22,7 +22,7 @@ import { COLORS, SIZES, icons } from "../../constants";
 import { useState } from "react";
 import useFetch from "../../hook/useFetch";
 
-const tabs = ["About", "Qualification", "Responsibilities"];
+const tabs = ["About", "Qualifications", "Responsibilities"];
 
 const JobDetails = () => {
   const params = useSearchParams();
@@ -36,6 +36,25 @@ const JobDetails = () => {
   const [activeTab, setActiveTab] = useState(tabs[0]);
 
   const onRefresh = () => {};
+
+  const displayTabContent = () => {
+    switch (activeTab) {
+      case "Qualifications":
+        return (
+          <Specifics
+            title="Qualifications"
+            points={data[0].job_highlights?.Qualifications ?? ["N/A"]}
+          />
+        );
+      case "About":
+        return (
+          <JobAbout info={data[0].job_description ?? "No data provided"} />
+        );
+      case "Responsibilities":
+      default:
+        break;
+    }
+  };
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
       <Stack.Screen
@@ -83,6 +102,7 @@ const JobDetails = () => {
                 activeTab={activeTab}
                 setActiveTab={setActiveTab}
               />
+              {displayTabContent()}
             </View>
           )}
         </ScrollView>
--- components/jobdetails/tabs/Tabs.jsx
+++ components/jobdetails/tabs/Tabs.jsx
@@ -4,16 +4,33 @@ import { View, Text, TouchableOpacity, FlatList } from "react-native";
 import styles from "./tabs.style";
 import { SIZES } from "../../../constants";
 
-const TabButton = () => {};
+function TabButton({ name, activeTab, onHandleSearchType }) {
+  return (
+    <TouchableOpacity
+      style={styles.btn(name, activeTab)}
+      onPress={onHandleSearchType}
+    >
+      <Text style={styles.btnText(name, activeTab)}>{name}</Text>
+    </TouchableOpacity>
+  );
+}
 
 const Tabs = ({ tabs, activeTab, setActiveTab }) => {
   return (
     <View style={styles.container}>
       <FlatList
         data={tabs}
-        renderItem={({ item }) => {
-          <TabButton />;
-        }}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item }) => (
+          <TabButton
+            name={item}
+            activeTab={activeTab}
+            onHandleSearchType={() => setActiveTab(item)}
+          />
+        )}
+        contentContainerStyle={{ columnGap: SIZES.small / 2 }}
+        keyExtractor={(item) => item}
       />
     </View>
   );
